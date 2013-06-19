706b0a977dc5ba2afe711049fcc5e19b0d3e4c4c
Rudy Grigar <rudy@grigar.net>
2013-06-19 11:48:58 -0700

    add the mod for checklist as well

---
+++ mod/checklist/importexportfields.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$separator = ',';
+
+// fieldname => output string
+$fields = array('displaytext' => 'Item text',
+                'indent' => 'Indent',
+                'itemoptional' => 'Type (0 - normal; 1 - optional; 2 - heading)',
+                'duetime' => 'Due Time (timestamp)',
+                'colour' => 'Colour (red; orange; green; purple; black)');
